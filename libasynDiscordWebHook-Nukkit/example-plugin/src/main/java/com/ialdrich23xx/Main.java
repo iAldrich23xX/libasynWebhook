9c0c7689c3c562963e38455a4b661275c67b6ad6
iAldrich23xX <wilsonalejandro83@gmail.com>
2023-03-24 06:01:43 -0500

    init example-plugin Nukkit

---
+++ libasynDiscordWebHook-Nukkit/example-plugin/src/main/java/com/ialdrich23xx/Main.java
@@ -0,0 +1,21 @@
+package com.ialdrich23xx;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class Main extends PluginBase {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
